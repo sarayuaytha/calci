ab8fd67113044e94b6d1a82d0fb7e2bb8d0b0b22
sarayuaytha <128344110+sarayuaytha@users.noreply.github.com>
2023-06-16 15:11:30 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,77 @@
+var ans=0;
+var s=""
+ document.getElementById("add").onclick=function()
+ {
+    var a =parseInt(document.getElementById("n1").value);
+    ans=ans+a;
+    if(s.length==0)
+    s=s+a;
+    else
+    s=s+("+"+a);
+    document.getElementById("result").textContent =ans;
+}
+document.getElementById("sub").onclick=function()
+{
+    var a =parseInt(document.getElementById("n1").value);
+    ans=ans+a;
+    if(s.length==0)
+    s=s+a;
+    else
+    s=s+("-"+a);
+    document.getElementById("result").textContent =ans;
+}
+
+
+document.getElementById("mul").onclick=function()
+{
+    var a =parseInt(document.getElementById("n1").value);
+    ans=ans+a;
+    if(s.length==0)
+    s=s+a;
+    else
+    s=s+("*"+a);
+    document.getElementById("result").textContent =ans;
+}
+
+document.getElementById("div").onclick=function()
+{
+    var a =parseInt(document.getElementById("n1").value);
+    ans=ans+a;
+    if(s.length==0)
+    s=s+a;
+    else
+    s=s+("/"+a);
+    document.getElementById("result").textContent =ans;
+}
+
+document.getElementById("answer").onclick=function(){
+    document.getElementById("wait").textContent=s;
+}
+/*add=()=>{
+    var a=parseInt(document.getElementById("n1").value);
+    var b=parseInt(document.getElementById("n2").value);
+    document.getElementById("result").textContent=(a+b);
+   /* console.log(n1+n2);
+}*/
+
+/*sub=()=>{
+    var a=parseInt(document.getElementById("n1").value);
+    var b=parseInt(document.getElementById("n2").value);
+    document.getElementById("result").textContent=(a-b);
+   /* console.log(n1-n2);
+}*/
+
+/*mul=()=>{
+    var a=parseInt(document.getElementById("n1").value);
+    var b=parseInt(document.getElementById("n2").value);
+    document.getElementById("result").textContent=(a*b);
+   /* console.log(n1*n2);
+}*/
+/*div=()=>{
+    var a=parseInt(document.getElementById("n1").value);
+    var b=parseInt(document.getElementById("n2").value);
+    document.getElementById("result").textContent=(a/b);
+   /* console.log(n1/n2);
+}*/
+
+
